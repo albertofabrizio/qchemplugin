94b242151972b753542b484eab2427ede8fe1338
Alberto Fabrizio <alberto@AlbertoPC.home>
2016-11-28 22:41:01 +0100

    minor changes to __init__.py

--- output_plugin/__init__.py
+++ output_plugin/__init__.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 from aiida.parsers.parser import Parser
 
-__copyright__ = u"Copyright (c), This file is part of the AiiDA platform. For further information please visit http://www.aiida.net/. All rights reserved."
-__license__ = "MIT license, see LICENSE.txt file."
-__version__ = "0.7.0"
-__authors__ = "The AiiDA team."
-
-class BasenwcParser(Parser):
+class QchemBaseParser(Parser):
 
     def parse_with_retrieved(self,retrieved):
         """
