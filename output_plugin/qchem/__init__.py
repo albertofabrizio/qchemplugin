8fb1d70da97e7dbbd8f8fc64c5735a8154bb2397
alberto.fabrizio <alberto.fabrizio@epfl.ch>
2016-11-30 17:08:04 +0100

    First Working Version Energy Only

--- output_plugin/qchem/__init__.py
+++ output_plugin/qchem/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from aiida.orm.data.parameter import ParameterData
 from aiida.parsers.parser import Parser
 
 class QchemBaseParser(Parser):
@@ -74,12 +75,6 @@ class QchemBaseParser(Parser):
         Extracts output nodes from the standard output and standard error
         files.
         """
-#       from aiida.orm.data.array.trajectory import TrajectoryData
-#       import re
-
-#       state= 'qchem-scf-module'
-#       step = None
-#       scale = None
         with open(output_path) as f:
             lines = [x.strip('\n') for x in f.readlines()]
 
@@ -90,31 +85,4 @@ class QchemBaseParser(Parser):
                 result_dict['energy']=line.split()[8]
                 continue
 
-
-#           if state is None and re.match('^\s*NWChem Geometry Optimization\s*$',line):
-#               state = 'nwchem-geometry-optimisation'
-#               trajectory = TrajectoryData()
-#               continue
-#           if state == 'nwchem-scf-module' and re.match('^\s*Final RHF \s*results\s*$',line):
-#               state = 'final-rhf-results'
-#               continue
-#           if re.match('^\s*\-*\s*$',line):
-#               continue
-#           if state == 'final-rhf-results':
-#               result = re.match('^\s*([^=]+?)\s*=\s*([\-\d\.]+)$',line)
-#               if result:
-#                   key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).lower())
-#                   result_dict[key] = result.group(2)
-#               else:
-#                   state = 'nwchem-scf-module'
-#           if state == 'nwchem-geometry-optimisation' and re.match('^\s*Step\s+\d+\s*$',line):
-#               result = re.match('^\s*Step\s+(\d+)\s*$',line)
-#               step = result.group(1)
-#               continue
-#           if state == 'nwchem-geometry-optimisation' and \
-#               re.match('^\s*Output coordinates in a.u.',line):
-#               state = 'nwchem-geometry-optimisation-coordinates'
-#               result = re.match('scale by \s(*[\-\d\.]+)',line)
-#               scale = result.group(1)
-#               continue
         return [('parameters', ParameterData(dict=result_dict))]
